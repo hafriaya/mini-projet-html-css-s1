f95744d23a8607d4d99239c2e45225ba49f1fc73
= <=>
2022-12-24 15:21:51 +0100

    show-room

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+var swiper = new Swiper(".featured-slider", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
